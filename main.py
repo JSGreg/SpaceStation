37536fbf2216a5b784a524965b99f876453f8313
Jonathan Skirtun <53845414+JSGreg@users.noreply.github.com>
2023-07-22 21:04:42 +1000

    Added pull for names
    Added a option to extract names from the space station

--- main.py
+++ main.py
@@ -5,20 +5,33 @@ import json
 
 def main ():
     userInput = input('What do you want to know? (Number, People)')
-
     userInput = userInput.lower()
 
     # Filters invalid responses
     if not userInput == "number" and not userInput == "people":
         return print("Invalid Input")
     
-    # API call
+    # API call    
     response = requests.get("http://api.open-notify.org/astros.json")
-    #turns into a dictionary
-    response=response.text
-
+    response = response.text
+    #turns response into a dictionary
     dict_object = json.loads(response)
 
+    if userInput == "people":
+        
+        userInput2=input("Do you want names?(Y/N)")
+        userInput2 = userInput2.lower()
+        
+        if userInput2 == "y":
+            nameStr = "name"
+            x = 0
+            for number in dict_object[userInput]:
+                
+                print(dict_object[userInput][x][nameStr])
+                x=x+1
+        return
+
+    # prints requested infomation
     print(dict_object[userInput])
 
 main()
