60a57ac89980ce311030a2eb8be4be9cbb30e2a4
Jonathan Skirtun <53845414+JSGreg@users.noreply.github.com>
2023-07-22 20:01:36 +1000

    Filter made, Input implemented
    Filter out invalid responses to user input

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import requests
 import json
 
-response = requests.get("http://api.open-notify.org/astros.json")
 
-print(response.json())
+
+def main ():
+    userInput = input('What do you want to know? (Number, People)')
+
+    userInput = userInput.lower()
+
+    # Filters invalid responses
+    if not userInput == "number" and not userInput == "people":
+        return print("Invalid Input")
+    
+    # API call
+    response = requests.get("http://api.open-notify.org/astros.json")
+    #turns into a dictionary
+    response=response.text
+
+    dict_object = json.loads(response)
+
+    print(dict_object[userInput])
+
+main()
