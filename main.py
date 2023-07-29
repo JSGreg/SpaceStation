b443e3e8d6aa6cb1e75d0e86c4c9963c968ad400
Jonathan Skirtun <53845414+JSGreg@users.noreply.github.com>
2023-07-29 17:56:50 +1000

    Made loop
    Moved main functions into while loop and created KeyError exception case

--- main.py
+++ main.py
@@ -1,37 +1,53 @@
 import requests
 import json
 
+# Could sperate into different functions?
+# Could also ask where coords? http://api.open-notify.org/iss-now.json
 
 
 def main ():
-    userInput = input('What do you want to know? (Number, People)')
-    userInput = userInput.lower()
-
-    # Filters invalid responses
-    if not userInput == "number" and not userInput == "people":
-        return print("Invalid Input")
-    
     # API call    
     response = requests.get("http://api.open-notify.org/astros.json")
     response = response.text
+    
     #turns response into a dictionary
     dict_object = json.loads(response)
+    
+    while True:
+        try: 
+            userInput = input('What do you want to know? (Number, People)')
+            userInput = userInput.lower()
 
-    if userInput == "people":
-        
-        userInput2=input("Do you want names?(Y/N)")
-        userInput2 = userInput2.lower()
-        
-        if userInput2 == "y":
-            nameStr = "name"
-            x = 0
-            for number in dict_object[userInput]:
+            # Filters invalid responses
+            # if not userInput == "number" and not userInput == "people":
+            #     print("Invalid Input")
                 
-                print(dict_object[userInput][x][nameStr])
-                x=x+1
-        return
 
-    # prints requested infomation
-    print(dict_object[userInput])
+            if userInput == "people":
+                userInput2=input("Do you want names?(Y/N)")
+                userInput2 = userInput2.lower()
+                if userInput2 == "y":
+                    nameStr = "name"
+            
+                    x = 0
+                    for number in dict_object[userInput]:
+                        print(dict_object[userInput][x][nameStr])
+                    x=x+1
+                elif userInput2 == "n":
+                    print ("No data.")
+                     
+                    
+            
+            # prints requested infomation
+            # print(dict_object[userInput])
+            break
+        except KeyError: 
+            print("Invalid Input")
+    
+
+
+    return
+
+    
 
 main()
