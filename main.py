28ee3f06c28d5b178bc2396a1a5525ff19cc51a3
Jonathan Skirtun <53845414+JSGreg@users.noreply.github.com>
2023-08-20 17:16:48 +1000

    Added func to find ISS
    Added function to find the ISS and open in google maps
    Restructured code and questions
    Fixed error handling

--- main.py
+++ main.py
@@ -1,53 +1,64 @@
 import requests
 import json
+from webbrowser import open
+from sys import argv
 
 # Could sperate into different functions?
 # Could also ask where coords? http://api.open-notify.org/iss-now.json
 
-
-def main ():
+def main():
     # API call    
     response = requests.get("http://api.open-notify.org/astros.json")
     response = response.text
     
     #turns response into a dictionary
     dict_object = json.loads(response)
+
+    nameStr = "name"
+    craftStr = "craft"
     
     while True:
         try: 
-            userInput = input('What do you want to know? (Number, People)')
+            print("Who is in space?")
+            userInput = input('What do you want to know? (Number/People)')
             userInput = userInput.lower()
 
-            # Filters invalid responses
-            # if not userInput == "number" and not userInput == "people":
-            #     print("Invalid Input")
-                
-
             if userInput == "people":
-                userInput2=input("Do you want names?(Y/N)")
-                userInput2 = userInput2.lower()
-                if userInput2 == "y":
-                    nameStr = "name"
+                for info in dict_object[userInput]:
+                    print("\n", info[nameStr], "who is on the", info[craftStr])
+        
+            elif userInput == "number":
+                print("There are currently", dict_object[userInput], "people in space.")
+            else:
+                raise KeyError ("Invalid Input")
             
-                    x = 0
-                    for number in dict_object[userInput]:
-                        print(dict_object[userInput][x][nameStr])
-                    x=x+1
-                elif userInput2 == "n":
-                    print ("No data.")
-                     
-                    
             
-            # prints requested infomation
-            # print(dict_object[userInput])
+            userInput2 = input('Do you want to know where the ISS is? (Y/N)')
+            userInput = userInput.lower()
+            
+            if userInput2 == "y":
+                map()
+            elif userInput2 == "n":
+                break
+            else:
+                raise KeyError("Invalid Input")
             break
+                    
         except KeyError: 
             print("Invalid Input")
-    
-
 
     return
 
+def map():
+    response = requests.get("http://api.open-notify.org/iss-now.json")
+    response = response.text
+    dict_object = json.loads(response)
     
+    address = dict_object["iss_position"]["latitude"]
+    address = address + " " + dict_object["iss_position"]["longitude"]
+    
+
+    open("http://www.google.com/maps/place/"+ address)
+    return
 
 main()
